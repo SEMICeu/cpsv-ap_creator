5851912461433c8f2e9fb4475cbe8f92fb8a45b8
Carmen Vandeloo <carmen_vandeloo@outlook.com>
2017-08-02 11:24:56 +0200

    reorganise github + install.bat

--- template/Text.js
+++
@@ -1,9 +0,0 @@
-/*global define*/
-define(["dojo/_base/declare", "./Item"], function(declare, Item) {
-
-    /**
-     * Same functionality as an Item, but separate class to make switching on type possible.
-     */
-    return declare(Item, {
-    });
-});
